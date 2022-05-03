4674eb89f4fa30226624b029c9f439557c374452
Anurag231000 <41607867+Anurag231000@users.noreply.github.com>
2022-05-03 23:32:27 +0530

    Update Diameter of Tree.java

--- Online GFG/Tree/Diameter of Tree.java
+++ Online GFG/Tree/Diameter of Tree.java
@@ -4,15 +4,15 @@ class Solution {
     int diameter(Node root) {
         
         if(root==null)
-			    return 0;
+	   return 0;
         
         heightOfTree(root);
         
         return res;
     }
     
-  /*T.C : Theta(n)
-  *A.S.C : Theta(h)
+  /*T.C : O(N*N) ( For every node, Height Function is called which takes O(N) time hence for every node it becomes N*N ) 
+  *A.S.C : O(1) ( Extra Space ) + O(H) ( Recursive Stack Space where “H” is the height of tree )
   * 
   *LOGIC : We get the height of left tree and right tree and last included with the root. In the end we return the maximum height among the 3 heights.
   */
@@ -28,3 +28,7 @@ class Solution {
 		return Math.max(lh,rh)+1;
 	}
 }
+
+/*OPTIMIZED SOLUTION
+1.Using Post Order Traversal : https://takeuforward.org/data-structure/calculate-the-diameter-of-a-binary-tree/
+*/
